80d1a891e0578552fd3a36707d0dfba2611168e6
davidlealo <davidlealo@gmail.com>
2022-10-19 20:55:02 -0300

    Aplicación terminada

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './App.css';
+import Input from './components/Input';
+import Boton from './components/Boton';
+
+function App() {
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState('')
+  return (
+    <div className="App">
+      <Input 
+      user={user}
+      setUser={setUser}
+      password={password}
+      setPassword={setPassword}
+      />
+      {password==="252525" && <Boton />}      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Boton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Boton() {
+  return (
+    <div>
+        <button>Ingresar</button>
+    </div>
+  )
+}
+
+export default Boton
